8e94c7a60bdaa6bc72fc708d0610c50c1deb2ee8
gtc <159605112+grahametc@users.noreply.github.com>
2026-03-22 13:43:29 -0400

    schyeah

---
+++ src/main.py
@@ -0,0 +1,40 @@
+import cv2 as cv
+import numpy as np
+
+
+def tst():
+    cap = cv.VideoCapture("sugar.mp4")
+
+    if cap.isOpened():
+        print("cont")
+    else:
+        print("Failure")
+
+    width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+    fourcc = cv.VideoWriter_fourcc(*'mp4v')
+    output = cv.VideoWriter("output.mp4", fourcc, 24, (width, height), isColor=True)
+    while True:
+        ret, frame = cap.read()
+    
+        if not ret: break
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        _, binary = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+        (contours, hierarchy) = cv.findContours(binary, cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+        if len(contours)>0:
+            contour= max(contours,key=cv.contourArea)
+            print(contour)
+            area = cv.contourArea(contour)
+            if area>800:
+                x,y,w,h = cv.boundingRect(contour)
+                cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                output.write(frame)
+       
+    cv.destroyAllWindows() 
+        
+    cap.release()
+    output.release()
+
+if __name__=="__main__":
+   tst()
